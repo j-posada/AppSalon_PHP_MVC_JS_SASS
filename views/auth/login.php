ad4b49e9ac18596b681c10d4df1c59c07b870c20
Juanra Posada <juanra.posada@gmail.com>
2024-05-02 08:48:12 +0200

    447. Creando el Formulario de Autenticación

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,29 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form action="/" method="POST" class="formulario">
+	<div class="campo">
+		<label for="email">Email</label>
+		<input
+			type="email"
+			id="email"
+			placeholder="tu@email.com"
+			name="email"
+		/>
+	</div>
+	<div class="campo">
+		<label for="password">Password</label>
+		<input
+			type="password"
+			id="password"
+			placeholder="tu password"
+			name="password"
+		/>
+	</div>
+	<input type="submit" class="boton" value="Iniciar Sesión">
+
+	<div class="acciones">
+		<a href="/crear-cuenta">Crea una nueva cuenta</a>
+		<a href="/olvide">Has olvidado tu contraseña?</a>
+	</div>
+</form>
