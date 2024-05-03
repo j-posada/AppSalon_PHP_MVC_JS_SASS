fad00858e69004d0de98a572c3ee5b5a3d01e0ee
Juanra Posada <juanra.posada@gmail.com>
2024-05-03 20:08:29 +0200

    460. Instanciando y sincronizando Usuario con el Formulario

--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -4,23 +4,52 @@
 <form class="formulario" method="POST" action="/crear-cuenta">
 	<div class="campo">
 		<label for="nombre">Nombre</label>
-		<input type="text" id="nombre" name="nombre" placeholder="Tu nombre" />
+		<input 
+			type="text" 
+			id="nombre" 
+			name="nombre" 
+			placeholder="Tu nombre" 
+			value="<?php echo s($usuario->nombre) ?>"
+		 />
 	</div>
 	<div class="campo">
 		<label for="apellido">Apellido</label>
-		<input type="text" id="apellido" name="apellido" placeholder="Tu apellido" />
+		<input 
+			type="text" 
+			id="apellido" 
+			name="apellido" 
+			placeholder="Tu apellido" 
+			value="<?php echo s($usuario->apellido) ?>"
+		 />
 	</div>
 	<div class="campo">
 		<label for="telefono">Teléfono</label>
-		<input type="tel" id="telefono" name="telefono" placeholder="Tu teléfono" />
+		<input 
+			type="tel" 
+			id="telefono" 
+			name="telefono" 
+			placeholder="Tu teléfono" 
+			value="<?php echo s($usuario->telefono) ?>"
+		 />
 	</div>
 	<div class="campo">
 		<label for="email">e-mail</label>
-		<input type="email" id="email" name="email" placeholder="Tu email" />
+		<input 
+			type="email" 
+			id="email" 
+			name="email" 
+			placeholder="Tu email" 
+			value="<?php echo s($usuario->email) ?>"
+		 />
 	</div>
 	<div class="campo">
 		<label for="password">Password</label>
-		<input type="password" id="password" name="password" placeholder="Tu password" />
+		<input 
+			type="password" 
+			id="password" 
+			name="password" 
+			placeholder="Tu password"
+		 />
 	</div>
 	<input type="submit" class="boton" value="Crear cuenta">
 </form>
