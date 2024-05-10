e142f17396f2ace91e6f92797919396d8f15c5b5
Juanra Posada <juanra.posada@gmail.com>
2024-05-10 20:29:58 +0200

    484. Creando Routing y Controllers para Citas

---
+++ views/cita/index.php
@@ -0,0 +1,6 @@
+<h1 class="nombre-pagina">Crear Nueva Cita</h1>
+<p class="descripcion-pagina">Elige tus servicios a continuación</p>
+
+<?php 
+include_once __DIR__ . "/../templates/alertas.php"
+?>
