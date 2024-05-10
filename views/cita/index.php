9a3d047aabc235ff46a1166d9e476cc8bd832a75
Juanra Posada <juanra.posada@gmail.com>
2024-05-10 21:24:29 +0200

    486. Mostrar el Nombre del usuario autenticado

--- views/cita/index.php
+++ views/cita/index.php
@@ -7,16 +7,47 @@ include_once __DIR__ . "/../templates/alertas.php"
 
 <div id="app">
 	<div id="paso-1" class="seccion">
-	<h2>Servicios</h2>
-	<p>Elige tus servicios a continuación</p>
-	<div id="servicios" class="listado-servicios"></div>	
+		<h2>Servicios</h2>
+		<p class="text-center">Elige tus servicios a continuación</p>
+		<div id="servicios" class="listado-servicios"></div>
 	</div>
 	<div id="paso-2" class="seccion">
-	<h2>Tus Datos y Cita</h2>
-	<p>Coloca tus datos y fecha de tu cita</p>
+		<h2>Tus Datos y Cita</h2>
+		<p class="text-center">Coloca tus datos y fecha de tu cita</p>
+		<form action="" class="formulario">
+			<div class="campo">
+				<label for="nombre">Nombre</label>
+				<input 
+					type="text" 
+					id="nombre" 
+					name="nombre" 
+					placeholder="Tu nombre" 
+					value="<?php echo s($nombre) ?>"
+					disabled
+				/>
+
+			</div>
+			<div class="campo">
+				<label for="fecha">Fecha</label>
+				<input 
+					type="date" 
+					id="fecha" 
+					name="fecha" 
+				/>
+			</div>
+			<div class="campo">
+				<label for="hora">Hora</label>
+				<input 
+					type="time" 
+					id="hora" 
+					name="hora" 
+				/>
+			</div>
+
+		</form>
 	</div>
 	<div id="paso-3" class="seccion">
-	<h2>Resumen</h2>
-	<p>Verifica la información antes de aceptar</p>
+		<h2>Resumen</h2>
+		<p class="text-center">Verifica la información antes de aceptar</p>
 	</div>
 </div>
