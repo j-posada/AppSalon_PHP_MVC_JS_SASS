6cdac78c49daffdc452df38864f9972be76642fe
Juanra Posada <juanra.posada@gmail.com>
2024-05-10 20:36:12 +0200

    485. Añadiendo los Contenedores Principales

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,6 +1,22 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
 <p class="descripcion-pagina">Elige tus servicios a continuación</p>
 
-<?php 
+<?php
 include_once __DIR__ . "/../templates/alertas.php"
 ?>
+
+<div id="app">
+	<div id="paso-1" class="seccion">
+	<h2>Servicios</h2>
+	<p>Elige tus servicios a continuación</p>
+	<div id="servicios" class="listado-servicios"></div>	
+	</div>
+	<div id="paso-2" class="seccion">
+	<h2>Tus Datos y Cita</h2>
+	<p>Coloca tus datos y fecha de tu cita</p>
+	</div>
+	<div id="paso-3" class="seccion">
+	<h2>Resumen</h2>
+	<p>Verifica la información antes de aceptar</p>
+	</div>
+</div>
