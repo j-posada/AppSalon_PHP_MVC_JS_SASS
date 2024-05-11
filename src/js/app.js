9ec4b3dfc54526d7ddef4f22638ad498188c7f64
j-posada <juanra.posada@gmail.com>
2024-05-11 13:37:31 +0200

    490. Primeros pasos con la Navegación por Tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,26 @@
-alert("hola");
+let paso= 1;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	iniciarApp();
+ });
+
+ function iniciarApp(){
+	tabs();
+ }
+
+ function mostrarSeccion(paso){
+	console.log ("dato: " + paso);
+ }
+
+ function tabs(){
+	const botones = document.querySelectorAll('.tabs button');
+
+	botones.forEach( boton => {
+		boton.addEventListener('click', function (e){
+			paso = parseInt(e.target.dataset.paso);
+			mostrarSeccion(paso);
+			//console.log("diste click en el botón: "+ boton.innerHTML);
+		});
+	});
+ }
