faeaab91337d4110d4e9a45c5d51e0908154f947
j-posada <juanra.posada@gmail.com>
2024-05-11 13:57:36 +0200

    491. Cambiando la sección según el Tab presionado

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,34 @@
-let paso= 1;
+let paso = 1;
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	iniciarApp();
- });
+});
 
- function iniciarApp(){
+function iniciarApp() {
 	tabs();
- }
+}
 
- function mostrarSeccion(paso){
-	console.log ("dato: " + paso);
- }
+function mostrarSeccion() {
 
- function tabs(){
+	// Ocular la sección actual
+	const seccionAnterior = document.querySelector(' .mostrar');
+	if (seccionAnterior) {
+		seccionAnterior.classList.remove('mostrar');
+	}
+	// Mostras la sección pulsada
+	const seccion = document.querySelector(`#paso-${paso}`);
+	seccion.classList.add('mostrar');
+}
+
+function tabs() {
 	const botones = document.querySelectorAll('.tabs button');
 
-	botones.forEach( boton => {
-		boton.addEventListener('click', function (e){
+	botones.forEach(boton => {
+		boton.addEventListener('click', function (e) {
 			paso = parseInt(e.target.dataset.paso);
-			mostrarSeccion(paso);
+			mostrarSeccion();
 			//console.log("diste click en el botón: "+ boton.innerHTML);
 		});
 	});
- }
+}
