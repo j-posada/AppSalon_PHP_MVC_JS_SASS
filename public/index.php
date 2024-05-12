096f96c70e33b3806d8fca2c760478101825a362
j-posada <juanra.posada@gmail.com>
2024-05-12 10:23:40 +0200

    496. Creando el Modelo de Servicios y exportando a JSON

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
@@ -32,6 +33,10 @@ $router->get('/confirmar-cuenta',[LoginController::class, 'confirmar']);
 
 //AREA PRIVADA
 $router->get('/cita',[CitaController::class, 'index']);
+
+//API DE Citas
+$router->get('/api/servicios',[APIController::class, 'index']);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
