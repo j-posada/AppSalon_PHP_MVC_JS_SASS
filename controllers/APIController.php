2d775b7c496da86b4b3f5801598d475cf9cfc01b
j-posada <juanra.posada@gmail.com>
2024-05-19 18:56:29 +0200

    516. Enviar peticiones al Servidor

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -11,4 +11,12 @@ class APIController
 		$servicios = Servicio::all();
 		echo json_encode($servicios);
 	}
+
+	public static function guardar(){
+
+		$respuesta =[
+			'mensaje' => 'Todo Ok'
+		];
+		echo json_encode($respuesta);
+	}
 }
