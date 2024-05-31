1ad4ab01f9721e86d3abb1e9b83946af2e3bdba5
j-posada <juanra.posada@gmail.com>
2024-05-31 08:34:43 +0200

    544. Creando las Vistas para los Servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -1,38 +1,61 @@
 <?php
 
 namespace Controllers;
+
 use MVC\Router;
 
 class ServicioController
 {
-	public static function index(Router $router){
+    public static function index(Router $router)
+    {
+        session_start();
+        isAdmin();
+        $alertas = [];
 
-		
-		session_start();
-		isAdmin();
-		$alertas = [];
-		
-		$router->render('servicios/index', [
-			'nombre' => $_SESSION['nombre'],
-			'id' => $_SESSION['id'],
-			'alertas' => $alertas
-		]); 
-	}	
+        $router->render('servicios/index', [
+            'nombre' => $_SESSION['nombre'],
+            'id' => $_SESSION['id'],
+            'alertas' => $alertas,
+        ]);
+    }
 
-	public static function crear(Router $router){
-		if ($_SERVER['REQUEST_METHOD' === 'POST']){
+    public static function crear(Router $router)
+    {
+        session_start();
+        isAdmin();
+        $alertas = [];
 
-		}
-	}
+        if ($_SERVER['REQUEST_METHOD' === 'POST']) {
 
-	public static function actualizar(Router $router){
-		if ($_SERVER['REQUEST_METHOD' === 'POST']){
-			
-		}
-	}
-	public static function eliminar(Router $router){
-		if ($_SERVER['REQUEST_METHOD' === 'POST']){
-			
-		}
-	}
+        }
+
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre'],
+            'id' => $_SESSION['id'],
+            'alertas' => $alertas,
+        ]);
+    }
+
+    public static function actualizar(Router $router)
+    {
+        session_start();
+        isAdmin();
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD' === 'POST']) {
+
+        }
+
+        $router->render('servicios/actualizar', [
+            'nombre' => $_SESSION['nombre'],
+            'id' => $_SESSION['id'],
+            'alertas' => $alertas,
+        ]);
+    }
+    public static function eliminar(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD' === 'POST']) {
+
+        }
+    }
 }
---
+++ views/servicios/crear.php
@@ -0,0 +1,6 @@
+<?php include_once __DIR__ . '/../templates/barra.php'?>
+<h1 class="nombre-pagina">Panel Admin</h1>
+<p class="descripcion-pagina">Crear servicios</p>
+<?php
+include_once __DIR__ . "/../templates/alertas.php"
+?>
