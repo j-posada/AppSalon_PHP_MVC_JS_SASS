94ac13e0deb54b82dc7b17626011332550272847
j-posada <juanra.posada@gmail.com>
2024-06-04 10:35:32 +0200

    548. CSS a los Servicios

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -10,6 +10,17 @@ foreach ($servicios as $servicio){ ?>
 <li>
 	<p>Nombre: <span><?php echo $servicio->nombre; ?></span></p>
 	<p>Precio: <span><?php echo moneyFormat($servicio->precio, 'ESP') . ' €' ; ?></span></p>
+	<div class="acciones">
+		<a class="boton boton-actualizar" href="/servicios/actualizar?id=<?php echo $servicio->id;?>">actualizar</a>
+		<form action="/servicios/eliminar" method="POST">
+			<input type="hidden"
+					name = "id"
+					value = "<?php echo $servicio->id ?>"
+			>
+			<input type="submit"  class="boton boton-eliminar" value="    borrar    ">
+		</form>
+
+		</div>
 </li>
 <?php } ?>
 </ul>
