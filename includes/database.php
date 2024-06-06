9df42f278253ad670db1345bdf7ec1fb4cb93c98
j-posada <juanra.posada@gmail.com>
2024-06-06 08:31:19 +0200

    553. Consideraciones para el Deployment y Variables de Entorno

--- includes/database.php
+++ includes/database.php
@@ -1,8 +1,14 @@
 <?php
 
 //$db = mysqli_connect('localhost', 'root', '', '');
-$db = new mysqli('65.21.107.92', 'test', 'tset', 'appsalon_mvc');
+$db = new mysqli(
+	$_ENV['DB_HOST'],
+	$_ENV['DB_USER'],
+	$_ENV['DB_PASS'],
+	$_ENV['DB_NAME']
+	);
 
+$db->set_charset('utf8');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
