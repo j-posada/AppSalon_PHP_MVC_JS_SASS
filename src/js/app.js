664048e76a50148ee54793f389625b305dcb74bc
j-posada <juanra.posada@gmail.com>
2024-06-06 09:01:21 +0200

    555. Rutas para Archivos Locales CSS y JS y creando un Build

--- src/js/app.js
+++ src/js/app.js
@@ -118,7 +118,7 @@ function bt_paginaAnterior() {
 async function consultarAPI() {
 
 	try {
-		const url = 'http://localhost:3000/api/servicios';
+		const url = `${location.origin}/api/servicios`;
 		const resultado = await fetch(url);
 		const servicios = await resultado.json();
 		mostrarServicios(servicios);
@@ -322,7 +322,7 @@ async function crearCita() {
 	//petición hacia la api
 
 	try {
-		const url = 'http://localhost:3000/api/citas';
+		const url = `${location.origin}/api/citas`;
 		const respuesta = await fetch(url, {
 			method: 'POST',
 			body: datos
